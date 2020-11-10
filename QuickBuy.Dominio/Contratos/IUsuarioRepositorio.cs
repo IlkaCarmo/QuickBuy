c2c13235f3a0db761b566c2af155bafd554bb4f6
Ilka Carmo <ilka_2009@ig.com.br>
2020-11-09 21:34:40 -0300

    DbContext e DbSet

--- QuickBuy.Dominio/Contratos/IUsuarioRepositorio.cs
+++ QuickBuy.Dominio/Contratos/IUsuarioRepositorio.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using QuickBuy.Dominio.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace QuickBuy.Dominio.Contratos {
-    interface IUsuarioRepositorio {
+    public interface IUsuarioRepositorio : IBaseRepositorio<Usuario> {
+
+
+
     }
 }
